8f2ade76d50e5c3321548c35618c2846edc4aae4
Марія <mashakuchma2009@gmail.com>
2022-11-05 17:48:48 +0200

    урок 5, бабуся/дідусь, батьки, дитина

--- main.py
+++ main.py
@@ -14,10 +14,17 @@ class Child(Parents):
         print(self.height)
         print(self.speed)
 max_roma_pavlo = Grandparents()
-        
-
-
-
+#lesson6
+import requests
+help(requests)
+print(requests.__cake__)
+print(requests.__name__)
+print(requests.__url__)
+print(type(3))
+ls = []
+for i in dir(ls):
+    print(i)
+print(dir(ls))
 
 
 
